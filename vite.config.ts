7026efe201740fca975bb4c3e5cb248ed75b4d9a
Lee young-hee <dudgml464@naver.com>
2024-07-11 00:33:42 +0900

    gh-pages setup commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '', // 루트 도메인에 배포되는 경우 빈 문자열
   plugins: [react()],
   resolve: {
     alias: {
